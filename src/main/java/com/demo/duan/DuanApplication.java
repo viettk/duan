c9afc03a2d627e6ebdfdd636a72cae5832ac051e
unknown <viettkph09818@fpt.edu.vn>
2021-12-06 17:59:25 +0700

    ok

--- src/main/java/com/demo/duan/DuanApplication.java
+++ src/main/java/com/demo/duan/DuanApplication.java
@@ -3,9 +3,19 @@ package com.demo.duan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class DuanApplication {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DuanApplication.class, args);
